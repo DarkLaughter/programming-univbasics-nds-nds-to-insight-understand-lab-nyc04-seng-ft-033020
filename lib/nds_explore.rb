5d1068e91e0824fed36674da0bfac1707fe8875d
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-18 16:09:35 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -2,7 +2,9 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 
+
 def pretty_print_nds(nds)
+  # Change the code below to pretty print the nds with pp
   pp directors_database
   nil
 end
